55b0066bf815aaff06acb80e3a113a3b615f3e6a
Abhinav Garg <abhinavgarg26@gmail.com>
2017-08-12 11:05:52 +0530

    edit ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,38 +1,51 @@
 class TicketsController < ApplicationController
 
-	before_action :set_project
-	before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+  before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
-	def new
-		@ticket = @project.tickets.build
-	end
+  def new
+    @ticket = @project.tickets.build
+  end
 
-	def create
-		@ticket = @project.tickets.build(ticket_params)
+  def create
+    @ticket = @project.tickets.build(ticket_params)
 
-		if @ticket.save
-			flash[:notice] = "Ticket has been created."
-			redirect_to [@project, @ticket]
-		else
-			flash.now[:alert] = "Ticket has not been created."
-			render "new"
-		end
-	end
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been created."
+      render "new"
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	private
+  def edit
+  end
 
-	def ticket_params
-		params.require(:ticket).permit(:name, :description)
-	end
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been updated."
+      render "edit"
+    end
+  end
 
-	def set_ticket
-		@ticket = @project.tickets.find(params[:id])
-	end
+  private
 
-	def set_project
-		@project = Project.find(params[:project_id])
-	end
+  def ticket_params
+    params.require(:ticket).permit(:name, :description)
+  end
+
+  def set_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
 end
