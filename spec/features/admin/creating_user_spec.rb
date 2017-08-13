5df121fec942dbb683df04807cc9070ad379360a
Abhinav Garg <abhinavgarg26@gmail.com>
2017-08-13 19:01:13 +0530

    Add

--- spec/features/admin/creating_user_spec.rb
+++ spec/features/admin/creating_user_spec.rb
@@ -13,4 +13,13 @@ end
     fill_in "Password", with: "password"
     click_button "Create User"
     expect(page).to have_content "User has been created."
-end end
+end 
+  scenario "when the new user is an admin" do
+          fill_in "Email", with: "admin@example.com"
+          fill_in "Password", with: "password"
+          check "Is an admin?"
+          click_button "Create User"
+          expect(page).to have_content "User has been created."
+          expect(page).to have_content "admin@example.com (Admin)"
+end
+end
