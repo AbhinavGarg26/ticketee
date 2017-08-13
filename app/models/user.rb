07d81ee599cddba15cabacb4460ac38afef479c2
Abhinav Garg <abhinavgarg26@gmail.com>
2017-08-13 20:23:26 +0530

    archive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Include default devise modules. Others availa# :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def to_s
   	"#{email} (#{admin? ? "Admin" : "User"})"
+  end
+
+  scope :excluding_archived, lambda { where(archived_at: nil) }
+
+  def archive
+  	self.update(archived_at: Time.now)
   end
 end
