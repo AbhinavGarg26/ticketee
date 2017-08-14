9a6ec158c1cc97af5798fc468a26e440d1e42f38
Abhinav Garg <abhinavgarg26@gmail.com>
2017-08-14 20:45:00 +0530

    Sign in archieve

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others availa# :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   def to_s
-  	"#{email} (#{admin? ? "Admin" : "User"})"
+    "#{email} (#{admin? ? "Admin" : "User"})"
   end
 
   scope :excluding_archived, lambda { where(archived_at: nil) }
 
+  def active_for_authentication?
+    super && archived_at.nil?
+  end
+
+  def inactive_message
+    archived_at.nil? ? super : :archived
+  end
+
   def archive
-  	self.update(archived_at: Time.now)
+    self.update(archived_at: Time.now)
   end
 end
