45ab0816d437fe3d88cf40ea5a2ba9bb3920ba32
Abhinav Garg <abhinavgarg26@gmail.com>
2017-08-19 17:16:21 +0530

    show project for permission

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,38 +1,38 @@
 class ProjectsController < ApplicationController
 
-	before_action :set_project, only: [:show, :edit, :update]
+  before_action :set_project, only: [:show, :edit, :update]
 
-	def index
-		@projects = Project.all
-	end
+  def index
+    @projects = policy_scope(Project)
+  end
 
-	def show
-		authorize @project, :show?
-	end
+  def show
+    authorize @project, :show?
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @project.update(project_params)
-			flash[:notice] = "Project has been updated."
-			redirect_to @project
-		else
-			flash.now[:notice] = "Project has not been updated."
-			render "edit"
-		end
-	end
+  def update
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:notice] = "Project has not been updated."
+      render "edit"
+    end
+  end
 
-	private
+  private
 
-	def set_project
-		@project = Project.find(params[:id])
-	rescue ActiveRecord::RecordNotFound
-		flash[:alert] = "The project you were looking for could not be found."
-		redirect_to projects_path
-	end
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
 
-	def project_params
-		params.require(:project).permit(:name, :description)
-	end
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
