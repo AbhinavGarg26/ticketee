50753270e5b399cd090828d3b7cd442587464039
Abhinav Garg <abhinavgarg26@gmail.com>
2017-08-26 22:18:39 +0530

    assign roles

--- spec/features/admin/managing_roles_spec.rb
+++ spec/features/admin/managing_roles_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Admins can manage a user's roles" do
   let!(:st3) { FactoryGirl.create(:project, name: "Sublime Text 3") }
   before do
     login_as(admin)
-end
+  end
   scenario "when assigning roles to an existing user" do
     visit admin_user_path(user)
     click_link "Edit User"
@@ -17,4 +17,15 @@ end
     click_link user.email
     expect(page).to have_content "Internet Explorer: Viewer"
     expect(page).to have_content "Sublime Text 3: Manager"
-end end
+  end
+  scenario "when assigning roles to a new user" do
+    visit new_admin_user_path
+    fill_in "Email", with: "newuser@ticketee.com"
+    fill_in "Password", with: "password"
+    select "Editor", from: "Internet Explorer"
+    click_button "Create User"
+    click_link "newuser@ticketee.com"
+    expect(page).to have_content "Internet Explorer: Editor"
+    expect(page).not_to have_content "Sublime Text 3"
+  end
+end
